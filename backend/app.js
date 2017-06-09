b3b0858faf0918c679abebc35f0fb47a48adf473
spongah <rocksolid801@gmail.com>
2017-06-09 11:28:54 -0700

    basic express server works

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+// load modules and libraries
+const express = require('express');
+const path = require('path');
+
+// set port number
+const port = process.env.PORT || 8080;
+
+// create express app
+const app = express();
+
+// set root web folder to "/public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// MAIN ROUTES
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// START SERVER
+app.listen(port, () => {
+  console.log('Server started on port ' + port);
+});
