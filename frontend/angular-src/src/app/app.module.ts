46f778add0c885bba9317c8e79fd22da7bb809dd
spongah <rocksolid801@gmail.com>
2017-06-15 13:14:06 -0700

    basic routing, landing page imported from UI-DESIGN. navbar hides on land component

--- frontend/angular-src/src/app/app.module.ts
+++ frontend/angular-src/src/app/app.module.ts
@@ -2,19 +2,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { LandComponent } from './components/land/land.component';
+
+const appRoutes: Routes = [
+  { path: '', component: LandComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: 'profile', component: ProfileComponent },
+
+  { path: '**', component: LandComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    RegisterComponent,
+    SigninComponent,
+    LogoutComponent,
+    ProfileComponent,
+    LandComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
